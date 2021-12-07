7126469a73bdc499505aeb03ad1f310a42767e9c
Alex <alexanderdmottram@outlook.com>
2021-12-07 17:32:24 +0700

    Add comments to deal instrumet and portfolio.

--- core/portfolio.py
+++ core/portfolio.py
@@ -1,5 +1,6 @@
 
 class Portfolio:
+    """An object for holding deals (a deal being an instrument and quantity)."""
 
     def __init__(self):
         self.deals = {}
@@ -9,10 +10,12 @@ class Portfolio:
         return '\n'.join([f'{key}: {val}' for key, val in self.deals.items()])
 
     def add_deal(self, deal):
+        """Adds a deal to the portfolio object."""
         self.deals[self.deal_counter] = deal
         self.deal_counter += 1
 
     def price(self, market_data_object):
+        """Prices all the deals given a market data object."""
         total_pv = 0
         for index, deal in self.deals.items():
             instr_npv = deal.instrument.price(market_data_object)
--- instruments/instrument.py
+++ instruments/instrument.py
@@ -3,7 +3,20 @@ from abc import ABC, abstractmethod
 
 class BaseInstrument(ABC):
     """All instruments should inherit off BaseInstrument to ensure price
-    is implemented."""
+    is implemented.
+
+    Additionally, there should be no stateful behaviour of instruments.
+    Avoiding stateful behaviour allows us to use factories to create
+    only a minimal set of instruments that can be priced without any
+    side effects.
+
+    If a deal would be amended to change the information of the underlying
+    instrument, this should instead create a new instrument with the new
+    parameters (or call an already existing instrument).
+
+    The use of factories to produce instruments will increase the efficiency
+    of caching within calculations.
+    """
 
     @abstractmethod
     def price(self, market_data_object):
